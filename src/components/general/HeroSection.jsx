0d8eef1a10f70d1b5cb75381174778b7f9d7374f
tbrown034 <tbrown034@gmail.com>
2023-04-21 16:50:24 -0500

    added heroTopText option and added some css styling to the hero Section

--- src/components/general/HeroSection.jsx
+++ src/components/general/HeroSection.jsx
@@ -1,28 +1,34 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Button, Icon } from "semantic-ui-react";
-import CallButton from "../buttons/CallButton"
+import CallButton from "../buttons/CallButton";
 import { Link } from "react-router-dom";
 
-
-const HeroSection = ({ backgroundImage, altImage, title, subtitle, showButtons}) => {
+const HeroSection = ({
+  backgroundImage,
+  altImage,
+  title,
+  subtitle,
+  showButtons,
+  showTopText, topText
+}) => {
   return (
-    <div className="quad-bg-color"
-
-    >
-      <Card className="quad-bg-color"> 
+    <div className="quad-bg-color">
+      <Card className="quad-bg-color">
         <Card.Img src={backgroundImage} alt={altImage} />
         <Card.ImgOverlay className="heroOverlay">
-          <Card.Title className="heroTitle">
-            {title}
-          </Card.Title>
-          <Card.Text>
-            {subtitle}
-          </Card.Text>
+          {showTopText && (
+            <div>
+              <Card.Text className="heroTopText primary-color">{topText}</Card.Text>{" "}
+            </div>
+          )}
+
+          <Card.Title className="heroTitle primary-color">{title}</Card.Title>
+          <Card.Text className="heroText primary-color">{subtitle}</Card.Text>
           {showButtons && (
             <div className="btnGroup">
               <Link to="/appointment">
-                <Button animated className="myButton">
+                <Button animated>
                   <Button.Content visible>
                     <Icon name="calendar" /> Make an Appointment
                   </Button.Content>
--- src/components/pages/About/HeroSection.jsx
+++ src/components/pages/About/HeroSection.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import HeroSection from "../../../components/general/HeroSection"
-import AboutPhoto from "../../../assets/images/practice.jpeg"
+import Photo from "../../../assets/images/practice.jpeg"
 
 const AboutPage = () => {
   return (
     <div>
       <HeroSection
-        backgroundImage={AboutPhoto}
+        backgroundImage={Photo}
         altImage="Two Doctors"
-        title="About Us"
-        subtitle="Learn about our mission and values"
+        title="Dr. Keith Brown DDS, FAGD"
+        subtitle="Dr. Brown is one of Chicagolands most experienced and dedicated dentists. With more than 40 years treating patients in Naperville, Il., he is a doctor you can trust."
+        showTopText={true}
+        topText="Meet Your Next Dentist"
       />
     </div>
   );
